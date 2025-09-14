ee75a692106ea02bb684e75dfe4c99b6d7e29fc2
Timothy A. Reed <deltatradingsystems@gmail.com>
2025-09-14 12:50:33 -0400

    Refactor Application
    Created the project structure and turned SerialForthGUI into a module

---
+++ app.py
@@ -0,0 +1,12 @@
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from SerialForthGUI.mainwindow import MainWindow
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = MainWindow()
+    widget.show()
+    sys.exit(app.exec())
