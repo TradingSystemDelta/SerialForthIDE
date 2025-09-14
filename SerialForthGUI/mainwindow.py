1fce60db57dac5592b7a609ad59a33a51666991b
Timothy A. Reed <deltatradingsystems@gmail.com>
2025-09-13 23:32:14 -0400

    Button Boilerplate
    Began adding the methods to trigger on Open File, Save As, Send Line, Send All buttons. Along with methods to trigger when there is a text or cursor update in the
    Program Editor

--- SerialForthGUI/mainwindow.py
+++ SerialForthGUI/mainwindow.py
@@ -2,6 +2,7 @@
 import sys
 
 from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QPushButton, QTextEdit, QFileDialog
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
@@ -15,6 +16,53 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # Set up button click handlers
+        self.ui.SendAll.clicked.connect(self.on_send_all_clicked)
+        self.ui.SendLine.clicked.connect(self.on_send_line_clicked)
+        self.ui.actionOpen.triggered.connect(self.open_file_dialog)
+        self.ui.actionSave_As.triggered.connect(self.save_as_file_dialog)
+
+        self.ui.ProgramEditor.textChanged.connect(self.on_program_editor_text_changed)
+        self.ui.ProgramEditor.cursorPositionChanged.connect(self.on_program_editor_cursor_position_changed)
+
+    def save_as_file_dialog(self):
+        file_path, _ = QFileDialog.getSaveFileName(self, "Save File As", "", "All Files (*)")
+        if file_path:
+            print("Selected file to save as:", file_path)
+            raise NotImplementedError("File saving functionality is not implemented yet.")
+
+    def open_file_dialog(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*)")
+        if file_path:
+            print("Selected file:", file_path)
+            raise NotImplementedError("File opening functionality is not implemented yet.")
+
+    def on_program_editor_cursor_position_changed(self):
+        text_edit = self.ui.ProgramEditor
+        cursor = text_edit.textCursor()
+        line = cursor.blockNumber() + 1  # blockNumber is zero-based
+        column = cursor.columnNumber() + 1  # columnNumber is zero-based
+        print(f"Cursor moved to line {line}, column {column}")
+        raise NotImplementedError("Cursor position change handling is not implemented yet.")
+
+    
+    def on_program_editor_text_changed(self):
+        text_edit = self.ui.ProgramEditor
+        text = text_edit.toPlainText()
+        print(f"Program Editor text changed. New text: {text}")
+        raise NotImplementedError("Program Editor text change handling is not implemented yet.")
+
+    
+    def on_send_line_clicked(self):
+        line_text = self.ui.lineEdit.text()
+        print(f"Send LINE clicked. Line: {line_text}")
+        raise NotImplementedError("Send LINE functionality is not implemented yet.")
+
+    def on_send_all_clicked(self):
+        text_edit = self.ui.ProgramEditor
+        text = text_edit.toPlainText()
+        print(f"Send ALL clicked. Text: {text}")
+        raise NotImplementedError("Send ALL functionality is not implemented yet.")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- SerialForthGUI/ui_form.py
+++ SerialForthGUI/ui_form.py
@@ -29,6 +29,10 @@ class Ui_MainWindow(object):
         MainWindow.setSizeIncrement(QSize(10, 10))
         self.actionOpen = QAction(MainWindow)
         self.actionOpen.setObjectName(u"actionOpen")
+        self.actionSave = QAction(MainWindow)
+        self.actionSave.setObjectName(u"actionSave")
+        self.actionSave_As = QAction(MainWindow)
+        self.actionSave_As.setObjectName(u"actionSave_As")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.centralwidget.setEnabled(True)
@@ -43,18 +47,20 @@ class Ui_MainWindow(object):
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.textEdit = QTextEdit(self.centralwidget)
-        self.textEdit.setObjectName(u"textEdit")
-        self.textEdit.setMinimumSize(QSize(600, 525))
-        self.textEdit.setSizeIncrement(QSize(10, 10))
+        self.ProgramEditor = QTextEdit(self.centralwidget)
+        self.ProgramEditor.setObjectName(u"ProgramEditor")
+        self.ProgramEditor.setMinimumSize(QSize(600, 525))
+        self.ProgramEditor.setSizeIncrement(QSize(10, 10))
+        self.ProgramEditor.setLineWidth(4)
+        self.ProgramEditor.setMidLineWidth(3)
 
-        self.horizontalLayout_3.addWidget(self.textEdit)
+        self.horizontalLayout_3.addWidget(self.ProgramEditor)
 
-        self.textBrowser = QTextBrowser(self.centralwidget)
-        self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setMinimumSize(QSize(250, 525))
+        self.SerialMonitor = QTextBrowser(self.centralwidget)
+        self.SerialMonitor.setObjectName(u"SerialMonitor")
+        self.SerialMonitor.setMinimumSize(QSize(250, 525))
 
-        self.horizontalLayout_3.addWidget(self.textBrowser)
+        self.horizontalLayout_3.addWidget(self.SerialMonitor)
 
 
         self.verticalLayout_2.addLayout(self.horizontalLayout_3)
@@ -67,11 +73,17 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout.addWidget(self.lineEdit)
 
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setMinimumSize(QSize(75, 25))
+        self.SendLine = QPushButton(self.centralwidget)
+        self.SendLine.setObjectName(u"SendLine")
+        self.SendLine.setMinimumSize(QSize(100, 25))
 
-        self.horizontalLayout.addWidget(self.pushButton)
+        self.horizontalLayout.addWidget(self.SendLine)
+
+        self.SendAll = QPushButton(self.centralwidget)
+        self.SendAll.setObjectName(u"SendAll")
+        self.SendAll.setMinimumSize(QSize(100, 25))
+
+        self.horizontalLayout.addWidget(self.SendAll)
 
 
         self.verticalLayout_2.addLayout(self.horizontalLayout)
@@ -82,13 +94,30 @@ class Ui_MainWindow(object):
         self.menubar.setGeometry(QRect(0, 0, 900, 23))
         self.menuFile = QMenu(self.menubar)
         self.menuFile.setObjectName(u"menuFile")
+        self.menuEdit = QMenu(self.menubar)
+        self.menuEdit.setObjectName(u"menuEdit")
+        self.menuView = QMenu(self.menubar)
+        self.menuView.setObjectName(u"menuView")
+        self.menuBuild = QMenu(self.menubar)
+        self.menuBuild.setObjectName(u"menuBuild")
+        self.menuTools = QMenu(self.menubar)
+        self.menuTools.setObjectName(u"menuTools")
+        self.menuTerminal = QMenu(self.menubar)
+        self.menuTerminal.setObjectName(u"menuTerminal")
         MainWindow.setMenuBar(self.menubar)
         self.statusBar = QStatusBar(MainWindow)
         self.statusBar.setObjectName(u"statusBar")
         MainWindow.setStatusBar(self.statusBar)
 
         self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
+        self.menubar.addAction(self.menuView.menuAction())
+        self.menubar.addAction(self.menuBuild.menuAction())
+        self.menubar.addAction(self.menuTools.menuAction())
+        self.menubar.addAction(self.menuTerminal.menuAction())
         self.menuFile.addAction(self.actionOpen)
+        self.menuFile.addAction(self.actionSave)
+        self.menuFile.addAction(self.actionSave_As)
 
         self.retranslateUi(MainWindow)
 
@@ -98,7 +127,23 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"SerialForthIDE", None))
         self.actionOpen.setText(QCoreApplication.translate("MainWindow", u"Open", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"SendLine", None))
+        self.actionSave.setText(QCoreApplication.translate("MainWindow", u"Save", None))
+        self.actionSave_As.setText(QCoreApplication.translate("MainWindow", u"Save As", None))
+        self.ProgramEditor.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Ubuntu Sans'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
+        self.SendLine.setText(QCoreApplication.translate("MainWindow", u"Send Line", None))
+        self.SendAll.setText(QCoreApplication.translate("MainWindow", u"Send All", None))
         self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
+        self.menuEdit.setTitle(QCoreApplication.translate("MainWindow", u"Edit", None))
+        self.menuView.setTitle(QCoreApplication.translate("MainWindow", u"View", None))
+        self.menuBuild.setTitle(QCoreApplication.translate("MainWindow", u"Build", None))
+        self.menuTools.setTitle(QCoreApplication.translate("MainWindow", u"Tools", None))
+        self.menuTerminal.setTitle(QCoreApplication.translate("MainWindow", u"Terminal", None))
     # retranslateUi
 
